ef2a41b6176ed02727bf14d4bc8fff41a35bd4d4
hieu294 <tranthehieu12>
2021-12-03 21:03:13 +0700

    update Product

--- ProjectWeb2/ProjectWeb2/Interfaces/IProductLogic.cs
+++ ProjectWeb2/ProjectWeb2/Interfaces/IProductLogic.cs
@@ -1,8 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ProjectWeb2.Models;
 
-public class Class1
+namespace ProjectWeb2.Interfaces
 {
-	public Class1()
-	{
-	}
+    public interface IProductLogic
+    {
+        Task<List<FoodItem>> GetAllFood();
+        Task<List<FoodItem>> GetFoodGQ();
+        Task<List<FoodItem>> GetFoodNE();
+        Task<List<FoodItem>> GetFoodById(string id);
+    }
 }
