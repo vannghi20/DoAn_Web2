2bb60cddfb20b04a8ee3cc5e9c0d13b7be954846
maitram1209 <maitram12092000@gmail.com>
2021-12-03 19:08:32 +0700

    trang lien he

---
+++ ProjectWeb2/ProjectWeb2/Interfaces/IContactLogic.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using ProjectWeb2.Models;
+
+namespace ProjectWeb2.Interfaces
+{
+    public interface IContactLogic
+    {
+        Task<bool> CreatNewMess(ContacUs contact);
+    }
+}
