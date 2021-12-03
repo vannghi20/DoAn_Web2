eab0a7b1e4c866f676a13822e9591abdc3960d43
maitram1209 <maitram12092000@gmail.com>
2021-12-03 18:34:08 +0700

    CreatNewProject

---
+++ ProjectWeb2/ProjectWeb2/Interfaces/IApiService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectWeb2.Interfaces
+{
+    public interface IApiService
+    {
+        Task<string> GetApi(string url);
+    }
+}
---
+++ ProjectWeb2/ProjectWeb2/Models/ContacUs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectWeb2.Models
+{
+    public class ContacUs
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Phải có tên")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Chiều dài không chính xác")]
+        [Display(Name = "TÊN KHÁCH")] // Label hiện thị
+        public string name { get; set; }
+        [Required]
+        [EmailAddress]
+        [Display(Name = "EMAIL")]
+        public string gmail { get; set; }
+        public string message { get; set; }
+        [Required]
+        [Phone]
+        [Display(Name = "Phone")]
+        public string sdt { get; set; }
+        public DateTime ngaytao { get; set; }
+    }
+}
