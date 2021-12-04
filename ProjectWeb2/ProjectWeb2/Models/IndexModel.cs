55d876f0589168d70943cbb4fc27e1523e962161
Van Nghi <19211tt1981@mail.tdc.edu.vn>
2021-12-04 21:01:42 +0700

    update

---
+++ ProjectWeb2/ProjectWeb2/Models/IndexModel.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectWeb2.Models
+{
+    public class IndexModel
+    {
+        public List<FoodItem> ProductNE;
+        public List<FoodItem> ProductGQ;
+    }
+}
